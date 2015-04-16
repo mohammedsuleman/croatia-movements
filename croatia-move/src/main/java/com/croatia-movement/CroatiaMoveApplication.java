222a2582a95cc0eb4f77d5bf3c47fa74c4c02089
suleman <suleman@inonso.com>
2015-04-16 23:04:37 +0530

    commiting everything

---
+++ croatia-move/src/main/java/com/croatia-movement/CroatiaMoveApplication.java
@@ -0,0 +1,12 @@
+package com.croatia-movement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroatiaMoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CroatiaMoveApplication.class, args);
+    }
+}
---
+++ croatia-move/src/test/java/com/croatia-movement/CroatiaMoveApplicationTests.java
@@ -0,0 +1,18 @@
+package com.croatia-movement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CroatiaMoveApplication.class)
+@WebAppConfiguration
+public class CroatiaMoveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
