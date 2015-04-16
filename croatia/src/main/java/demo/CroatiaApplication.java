d273bd5601d897a92127fde19ba11d94cca7ce08
suleman <suleman@inonso.com>
2015-04-17 00:39:07 +0530

    commiting project

--- croatia/src/main/java/demo/CroatiaApplication.java
+++ croatia/src/main/java/demo/CroatiaApplication.java
@@ -1,12 +1,12 @@
-package com.croatia-movement;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CroatiaMoveApplication {
+public class CroatiaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CroatiaMoveApplication.class, args);
+        SpringApplication.run(CroatiaApplication.class, args);
     }
 }
--- croatia/src/test/java/demo/CroatiaApplicationTests.java
+++ croatia/src/test/java/demo/CroatiaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.croatia-movement;
+package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CroatiaMoveApplication.class)
+@SpringApplicationConfiguration(classes = CroatiaApplication.class)
 @WebAppConfiguration
-public class CroatiaMoveApplicationTests {
+public class CroatiaApplicationTests {
 
 	@Test
 	public void contextLoads() {
